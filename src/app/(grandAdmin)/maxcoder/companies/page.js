b00d9368f40e1ffc1e119537889c7c6953e85532
maxcoders <39728751+maxcoders@users.noreply.github.com>
2023-09-08 16:37:07 +0700

    Update page.js

--- src/app/(grandAdmin)/maxcoder/companies/page.js
+++ src/app/(grandAdmin)/maxcoder/companies/page.js
@@ -1,17 +1,4 @@
-import Empty from "./Empty";
-import List from "./List";
-import set from "@/api/set";
-const getFetch = async function (d) {
-  const t = await fetch(set.postdata + "?" + d, {
-    cache: "no-store",
-    //next: { revalidate: 0 },
-  });
-  const z = await t.json();
-  return z;
+const getFetch = function (d) {
+
+  return '';
 };
-export default async function F() {
-  const companies = await getFetch("a=maxcoderGetCompanies");
-  if (companies)
-    return <div>{companies && <List companies={companies} />}</div>;
-  if (!companies) return <div>{companies && <Empty />}</div>;
-}
