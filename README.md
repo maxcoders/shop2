e2ebf323031c4dd7ed6f52bb8186f79d087016ae
maxcoder <p2pcon102@gmail.com>
2023-09-10 13:40:13 +0700

    msg

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## Getting Started with git
+
+git add .
+git status
+git commit -m 'msg '
+git push
+
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/app/(front)/Footer.js
+++ src/app/(front)/Footer.js
@@ -2,9 +2,9 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg10 fr g10 p10 cw">
-      <Link href="http://localhost:3000">Shop</Link>
-      <Link href="http://localhost:3000/sellercenter">extranet</Link>
-      <Link href="http://localhost:3000/maxcoder">maxcoder</Link>
+      <Link href="/">Shop</Link>
+      <Link href="/sellercenter">extranet</Link>
+      <Link href="/maxcoder">maxcoder</Link>
     </footer>
   );
 }
